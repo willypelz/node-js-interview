abb0578ac9e98a8a64a813ac141b3e36aa7198ab
willypelz <pelumiasefon@gmail.com>
2021-03-13 17:35:51 +0100

    [IMP] udpate the project configuration

--- config.js
+++ config.js
@@ -3,9 +3,10 @@ require("dotenv").config({silent: true});
 
 
 module.exports = {
-    port: process.env.PORT || 8099,
+    port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || "development",
-    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/members_management",
+    mongoURI: process.env.MONGO_URI || "mongodb://database:27017/members_management",
+    // mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/members_management",
     secretKey: process.env.SECRET_KEY || '_secret_key_',
     defaultRecordsPerPage: process.env.DEFAULT_RECORDS_PER_PAGE || 10,
     defaultPage: process.env.DEFAULT_PAGE || 1,
