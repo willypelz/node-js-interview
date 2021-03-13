8a0c61efdd146d1a4f353fca5d6273ccbd9e4b83
willypelz <pelumiasefon@gmail.com>
2021-03-13 10:18:43 +0100

    [IMP] add project configuration

---
+++ config.js
@@ -0,0 +1,15 @@
+// Import env package
+require("dotenv").config({silent: true});
+
+
+module.exports = {
+    port: process.env.PORT || 8099,
+    env: process.env.NODE_ENV || "development",
+    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/members_management",
+    secretKey: process.env.SECRET_KEY || '_secret_key_',
+    defaultRecordsPerPage: process.env.DEFAULT_RECORDS_PER_PAGE || 10,
+    defaultPage: process.env.DEFAULT_PAGE || 1,
+    sentryDsn: process.env.SENTRY_DSN, //|| "https://b0ea4cba4df14adb8087a5c20f3bfe6b@o495019.ingest.sentry.io/5567082",
+    authSecret: process.env.AUTH_SECRET || '_secret_key_',
+    mqServerUrl: process.env.MQ_SERVER_URL || 'amqp://localhost'
+};
